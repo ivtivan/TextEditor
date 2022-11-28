63f4bd5183b37d3bebe02d45d599ac7e732aea46
Ivan <ivt.ivanov1@gmail.com>
2022-11-28 08:07:58 +0100

    initial setup with menu

---
+++ src/main/java/com/iviv/texteditor/Main.java
@@ -0,0 +1,9 @@
+package com.iviv.texteditor;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        new TextEditor();
+    }
+}
---
+++ src/main/java/com/iviv/texteditor/TextEditor.java
@@ -0,0 +1,69 @@
+package com.iviv.texteditor;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+
+public class TextEditor extends JFrame implements ActionListener {
+    JMenuItem open;
+    JMenuItem save;
+    JTextArea textArea;
+
+    public TextEditor() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Text Editor");
+        setVisible(true);
+        setSize(600, 600);
+        setJMenuBar(constructMenuBar());
+    }
+
+    private JMenuBar constructMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        open = new JMenuItem("Open");
+        save = new JMenuItem("Save");
+
+        open.addActionListener(this);
+        save.addActionListener(this);
+
+        menu.add(open);
+        menu.add(save);
+        
+        menuBar.add(menu);
+        return menuBar;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == open) {
+
+        }
+        else if (e.getSource() == save) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            if (fileChooser.showSaveDialog(fileChooser) == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                PrintWriter fileOut = null;
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println();
+                }
+                catch (FileNotFoundException exc) {
+                    System.out.println("Couldn't save file.");
+                }
+                finally {
+                    if (fileOut != null) {
+                        fileOut.close();
+                    }
+                }
+            }
+        }
+    }
+}
